f1d00861d3379d01319cef1a66619f3cec2ce9d9
Elijah Loy <loy.19@wright.edu>
2025-03-08 18:37:44 -0500

    Changed visuals for pantry and a button to add ingredients manually

--- pantry-pal/src/app/pantry/pantry.component.ts
+++ pantry-pal/src/app/pantry/pantry.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { NgIf, NgFor} from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-pantry',
-  standalone: true,
-  imports: [NavbarComponent],
-  templateUrl: './pantry.component.html',
-  styleUrl: './pantry.component.css'
+  	selector: 'app-pantry',
+  	standalone: true,
+  	templateUrl: './pantry.component.html',
+  	styleUrl: './pantry.component.css',
+	imports: [NavbarComponent, NgIf, NgFor, FormsModule]
 })
-export class PantryComponent {
 
+export class PantryComponent {
+	showPopup = false;
+	newIngredient = { name: '', quantity: 1, unit: '', type: 'misc' };
+	ingredients: { [key: string]: { name: string; quantity: number; unit: string }[] } = {
+		fruits: [], vegetables: [], grains: [], protein: [], dairy: [], season: [], sub: [], misc: []
+	};
+
+	toggleFoodForm() {
+		if (this.showPopup == true){
+			this.showPopup = false;
+		} else{
+			this.showPopup = true;
+		}
+	}
+  
+	addIngredient() {
+		if (this.newIngredient.name.trim() && this.newIngredient.unit.trim()) {
+			this.ingredients[this.newIngredient.type].push({ 
+		  		name: this.newIngredient.name, 
+		  		quantity: this.newIngredient.quantity, 
+		  		unit: this.newIngredient.unit 
+			});
+			this.newIngredient = { name: '', quantity: 1, unit: '', type: 'misc' };
+			this.showPopup = false;
+		}
+	}	
 }
